7b5733338df1eaad1139e6bea56c06a9e51dd148
annedien <annedient@gmail.com>
2016-10-05 10:20:33 +0200

    pizzeria

---
+++ test kopie.rb
@@ -0,0 +1,2 @@
+Piet ={fur: "black", age: 2, name: "Piet", amount_of_eyes: 2}
+Phra ={fur: "grey", age: 8, name: "Phra", amount_of_eyes: 2}
---
+++ waiter.rb
@@ -0,0 +1,6 @@
+#create a waiter
+class Waiter
+  def greet_guest
+  puts "Good day. Welkom to our lovely restaurant"
+  end
+end
